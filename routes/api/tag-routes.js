6077dc1439491ce214d3abebc54aa14841423a3a
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-01-05 00:03:37 -0800

    fixed variable name typo in 'get /' routes. Added '/:id' routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,18 +7,26 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const categoryData = await Tag.findAll({
+    const tagData = await Tag.findAll({
       include: [{ model: Product }]
     });
-    res.status(200).json(categoryData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
